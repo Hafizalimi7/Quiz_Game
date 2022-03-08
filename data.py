8b5c0190fc21996bcf44630dd6d5846ae9e55ca6
hafiz <hafizalimi77@gmail.com>
2022-03-08 10:01:22 +0000

    init

---
+++ data.py
@@ -0,0 +1,56 @@
+question_data =[
+        {"category": "General Knowledge",
+         "type": "boolean", "difficulty": "easy",
+         "question": "The Great Wall of China is visible from the moon.",
+         "correct_answer": "False",
+         "incorrect_answers": ["True"]
+         },
+        {"category": "General Knowledge",
+         "type": "boolean", "difficulty": "easy",
+         "question": "It is automatically considered entrapment in the United States if the police sell you illegal substances without revealing themselves.",
+         "correct_answer": "False", 
+         "incorrect_answers": ["True"]
+         },
+        {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+         "question": "On average, at least 1 person is killed by a drunk driver in the United States every hour.",
+         "correct_answer": "True", 
+         "incorrect_answers": ["False"]
+         },
+        {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+         "question": "When you cry in space, your tears stick to your face.", 
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]
+         },
+        {"category": "General Knowledge",
+         "type": "boolean", "difficulty": "easy",
+         "question": "The Lego Group was founded in 1932.", 
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]
+         },
+        {"category": "General Knowledge", "type": "boolean", "difficulty": "easy",
+         "question": "One of Donald Trump&#039;s 2016 Presidential Campaign promises was to build a border wall between the United States and Mexico.",
+         "correct_answer": "True", 
+         "incorrect_answers": ["False"]
+         },
+        {"category": "General Knowledge", 
+         "type": "boolean", "difficulty": "easy",
+         "question": "&quot;27 Club&quot; is a term used to refer to a list of famous actors, musicians, and artists who died at the age of 27.",
+         "correct_answer": "True", 
+         "incorrect_answers": ["False"]
+         },
+        {"category": "General Knowledge", 
+         "type": "boolean", "difficulty": "easy",
+         "question": "Dihydrogen Monoxide was banned due to health risks after being discovered in 1983 inside swimming pools and drinking water.",
+         "correct_answer": "False", 
+         "incorrect_answers": ["True"]
+         },
+        {"category": "General Knowledge", 
+         "type": "boolean", "difficulty": "easy",
+         "question": "The National Animal of Scotland is the Unicorn.", "correct_answer": "True",
+         "incorrect_answers": ["False"]},
+        {"category": "General Knowledge", 
+         "type": "boolean", "difficulty": "easy",
+         "question": "Slovakia is a member of European Union-",
+         "correct_answer": "True",
+         "incorrect_answers": ["False"]}
+]
